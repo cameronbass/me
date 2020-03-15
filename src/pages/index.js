ae2c42000deba08dfd07cd17704856eb22cf130a
Cameron Bass <cam@collectiveidea.com>
2020-03-15 12:22:08 -0400

    Add Droid Sans and more responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,8 +15,13 @@ const IndexPage = () => (
     <div class="content-container">
       <h1 class="greeting">Bonjour,</h1>
       <p class="sub-greeting"> Allow me to introduce myself: </p>
+      <div class="separator-container">
+        <div class="separator left"></div>
+        <div class="separator center"></div>
+        <div class="separator right"></div>
+      </div>
       <p>
-        I'm baby tofu tacos hammock pour-over, kinfolk lumbersexual asymmetrical banh mi literally street art tbh tousled raclette air plant. Mumblecore everyday carry chambray trust fund austin, venmo bushwick chicharrones vice. Deep v photo booth lomo pork belly next level hexagon fanny pack fixie cronut letterpress skateboard kitsch intelligentsia. Umami echo park mumblecore, polaroid hexagon asymmetrical stumptown fam.
+        I'm baby tofu tacos hammock pour-over, kinfolk asymmetrical banh mi literally street art tbh tousled raclette air plant. Mumblecore everyday carry chambray trust fund austin, venmo bushwick chicharrones vice. Deep v photo booth lomo pork belly next level hexagon fanny pack fixie cronut letterpress skateboard kitsch intelligentsia. Umami echo park mumblecore, polaroid hexagon asymmetrical stumptown fam.
       </p>
       <p>
         Copper mug craft beer food truck affogato irony. Kickstarter hexagon enamel pin wayfarers williamsburg tumeric godard literally single-origin coffee pug. Occupy organic raw denim yuccie. Fam flexitarian iPhone microdosing, chartreuse meditation kale chips. Leggings sustainable put a bird on it tote bag squid bespoke hell of mustache tattooed. Selvage next level iceland, whatever farm-to-table man bun brooklyn.
