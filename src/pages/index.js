22f9107d63e3f2a00bc1dc44c205ee94b5b20165
Cameron Bass <cam@collectiveidea.com>
2021-10-03 20:41:52 -0400

    Update placeholder

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,10 +21,10 @@ const IndexPage = () => (
         <div class="separator right"></div>
       </div>
       <p>
-        I'm baby tofu tacos hammock pour-over, kinfolk asymmetrical banh mi literally street art tbh tousled raclette air plant. Mumblecore everyday carry chambray trust fund austin, venmo bushwick chicharrones vice. Deep v photo booth lomo pork belly next level hexagon fanny pack fixie cronut letterpress skateboard kitsch intelligentsia. Umami echo park mumblecore, polaroid hexagon asymmetrical stumptown fam.
+        Placeholder Text
       </p>
       <p>
-        Copper mug craft beer food truck affogato irony. Kickstarter hexagon enamel pin wayfarers williamsburg tumeric godard literally single-origin coffee pug. Occupy organic raw denim yuccie. Fam flexitarian iPhone microdosing, chartreuse meditation kale chips. Leggings sustainable put a bird on it tote bag squid bespoke hell of mustache tattooed. Selvage next level iceland, whatever farm-to-table man bun brooklyn.
+        Placeholder Text
       </p>
     </div>
   </Layout>
